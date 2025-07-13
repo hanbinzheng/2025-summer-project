42a014d8da0b780fa4ad471636e0fe48cd94d077
hanbinzheng <hanbinzheng54@gmail.com>
2025-07-14 04:14:00 +0800

    update paths

--- paths/__init__.py
+++ paths/__init__.py
@@ -2,4 +2,7 @@ from .base import ConditionalPath
 from .gaussian import Alpha, Beta, GaussianConditionalPath
 from .linear_gaussian import LinearAlpha, LinearBeta
 
-__all__ = ["ConditionalPath", "Alpha", "Beta", "GaussianConditionalPath", "LinearAlpha", "LinearBeta"]
+__all__ = [
+    "ConditionalPath", "GaussianConditionalPath",
+    "Alpha", "Beta", "LinearAlpha", "LinearBeta"
+]
